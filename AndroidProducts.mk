b4c3b1a88ba9e076c16ba2365c17df5303580100
Zeref-Igris <igriszeref.002@gmail.com>
2024-06-16 13:57:30 +0530

    Welcome AwakenOS
    Signed-off-by: Zeref-Igris <igriszeref.002@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX2061.mk
+    $(LOCAL_DIR)/awaken_RMX2061.mk
+
+COMMON_LUNCH_CHOICES := \
+    awaken_RMX2061-user \
+    awaken_RMX2061-userdebug \
+    awaken_RMX2061-eng
